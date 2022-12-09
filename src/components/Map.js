340ea8e7fd7133ee90aa71716b664b979a0fa6bd
rodd7 <69749721+rodd7@users.noreply.github.com>
2022-12-09 22:00:21 +1100

    init

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Map(){
+    return(
+        <div>
+            <Map>
+                <p>this is a test</p>
+            </Map>
+        </div>
+    );
+}
+
+export default Map;
