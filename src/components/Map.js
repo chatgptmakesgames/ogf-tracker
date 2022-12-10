9d55b4fc0bcc00157a2b0378fe9c4db3139277c6
rodd7 <69749721+rodd7@users.noreply.github.com>
2022-12-11 00:44:39 +1100

    fetch
    works but arr not showing up on polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,53 +2,44 @@ import React from "react";
 import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import "./Map.css";
 
-function callRelation(id) {
-	(async () => {
-		const api = await fetch(
-			"https://overpass.ogf.rent-a-planet.com/api/interpreter",
-			{
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-				},
-				body: "[out:json];rel(" + id + ");(._;>;);out;",
-			},
-            
-		);
+async function callRelation(id) {
+    const api = await fetch("https://www.overpass-api.de/api/interpreter?", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: "[out:json];rel(" + id + ");(._;>;);out;",
+    });
 
-		const request = await api.json();
-        let path = [];
-		// console.log(request.elements);
+    const request = await api.json();
+    let path = [];
 
-        for (let pair of request.elements){
-            if (pair.type === "node"){
-                path.push([pair.lat, pair.lon]);
-            }    
+    for (let pair of request.elements) {
+        if (pair.type === "node") {
+            path.push([pair.lat, pair.lon]);
         }
-        console.log(path)
+    }
+    return path;
+}
 
-	})();
+async function retreive() {
+    let test1 = await callRelation(188466);
+    console.log(test1);
+    return test1;
 }
 
 const limeOptions = {color:'lime'}
 const test = [[1,2],[3,1]]
-console.log(test)
-let test1 = callRelation(188466);
 
-console.log(test1);
-
-
-export default function FlightMap() {
+export default function Map() {
 	return (
 		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
 			<TileLayer
-				/*attribution='<a href="//opengeofiction.net">OpenGeofiction</a> contributors (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-NC-SA</a>)'
-				url="https://tile.opengeofiction.net/ogf-carto/{z}/{x}/{y}.png"*/
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution='<a href="//opengeofiction.net">OpenGeofiction</a> contributors (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-NC-SA</a>)'
+				url="https://tile.opengeofiction.net/ogf-carto/{z}/{x}/{y}.png"
 			/>
-            <Polyline pathOptions={limeOptions} positions={test}/>
+            <Polyline pathOptions={limeOptions} positions={retreive()}/>
 		</MapContainer>
 	);
 }
